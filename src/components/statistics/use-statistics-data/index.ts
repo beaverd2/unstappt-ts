8f44004e4a9e868baccd2804f9851b92baf4fcda
beaverd2 <beaverrd2@gmail.com>
2022-09-10 02:01:14 +0300

    fix statistics reset

--- src/components/statistics/use-statistics-data/index.ts
+++ src/components/statistics/use-statistics-data/index.ts
@@ -22,6 +22,16 @@ export const useStatisticsData = (beers: Beer[]) => {
         avgRating: Number((beers.reduce((acc, cur) => acc + cur.userRating, 0) / beers.length).toPrecision(3)),
       })
     }
+    return () => {
+      setStatistics({
+        checkinsCount: 0,
+        beersCount: 0,
+        stylesCount: 0,
+        breweriesCount: 0,
+        countriesCount: 0,
+        avgRating: 0,
+      })
+    }
   }, [beers])
   return [statistics]
 }
