6336cced1d8349d57fe16b8636bc7d3376199399
beaverd2 <beaverrd2@gmail.com>
2025-05-11 21:17:21 +0300

    feat: fix width

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { SearchInput } from '@/components/search-input'
 
 export default function Page() {
   return (
-    <div className="flex flex-1 flex-col items-center justify-center gap-4">
+    <div className="flex w-full flex-1 flex-col items-center justify-center gap-4">
       <div className="w-full max-w-md">
         <SearchInput placeholder="Enter username" className="w-full bg-white" />
       </div>
