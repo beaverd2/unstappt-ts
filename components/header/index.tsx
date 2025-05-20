6759b9ab9810214db705f473b9bab6dcdb30033d
beaverd2 <beaverrd2@gmail.com>
2025-05-20 21:00:23 +0300

    feat: change header styles

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -4,9 +4,9 @@ import { ThemeToggle } from '@/components/theme-toggle'
 
 export const Header = () => {
   return (
-    <div className="w-full bg-primary py-2 md:py-4">
+    <div className="w-full border-b border-border py-2 md:py-4">
       <div className="container mx-auto flex items-center justify-between gap-4 px-4">
-        <Link className="text-xl font-semibold md:text-4xl" href="/" passHref>
+        <Link className="text-2xl font-semibold text-primary md:text-4xl" href="/" passHref>
           Unstappt
         </Link>
         <div className="flex items-center gap-4">
